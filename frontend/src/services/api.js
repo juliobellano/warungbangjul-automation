598bc393756a5d589c5f03eb103dfbb055efad9e
juliobellano <juliobellanolaksana@gmail.com>
2025-03-25 16:34:57 +0800

    added index.py at backend api to fetch data

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://warungbangjul-automation-jq8g36ele.vercel.app/';
+const API_BASE_URL = '/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
