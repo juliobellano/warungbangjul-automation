8d7944bd3e3e96bf5bf7ceeefddfb71178997217
juliobellano <juliobellanolaksana@gmail.com>
2025-04-08 22:55:08 +0800

    fixing bugs

--- frontend2.0/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Configure image domains if needed
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-};
-
-module.exports = nextConfig; 
